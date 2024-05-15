b252979a8b9deb7753fdc76784888b10052c1aa6
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-05-15 19:01:25 +0530

    try4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,19 +4,21 @@ public class Main {
     
     public static void main(String[] args) {
         Product p = new Product();
-        p.Product(5, 4);
-        p.Product(5, 3, 2);
-        p.Product(5.0, 7.0);
+
+        
+        p.product(5, 4);
+        p.product(5, 3, 2);
+        p.product(5.0, 7.0);
 
     }
     public static class Product{
-        public int Product(int x,int y){
+        public static int product(int x,int y){
             return x*y;
         }
-        public int Product(int x,int y,int z){
+        public static int product(int x,int y,int z){
             return x*y*z;
         }
-        public double Product(double x,double y){
+        public static double product(double x,double y){
             return x*y;
         }
     }
